c1fccdc3aedb1278e895b8ecd92b48bd4f5602eb
Jantay <jakcckaj12@gmail.com>
2022-07-29 15:35:34 +0600

    Added Repository, IRepository and UnitOfWork

---
+++ AutoMarket/AutoMarket.DAL/Data/Repository.cs
@@ -0,0 +1,70 @@
+﻿using AutoMarket.DAL.Interfaces;
+using AutoMarket.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.DAL.Data
+{
+    /// <summary>
+    /// Generic Repository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Repository<T> : IRepository<T>
+                where T : class, IEntity<int>
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> _set;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _set = _context.Set<T>();
+        }
+
+        public async Task<T> CreateAsync(T model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+
+            await _set.AddAsync(model);
+            return model; ;
+        }
+
+        public void Delete(T model)
+        {
+            if (_set.Any(x => x.Id == model.Id))
+            {
+                _set.Remove(model);
+            }
+        }
+
+        public void EditAsync(T model)
+        {
+            if (_set.Any(x => x.Id == model.Id))
+            {
+                _set.Update(model);
+            }
+        }
+
+        public async Task<List<T>> GetAsync()
+        {
+            var items = _set.AsQueryable();
+            return await items.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _set.FindAsync(id);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ AutoMarket/AutoMarket.DAL/Data/UnitOfWork.cs
@@ -0,0 +1,98 @@
+﻿using AutoMarket.DAL.Models;
+using AutoMarket.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarket.DAL.Data
+{
+    /// <summary>
+    /// Класс UnitOfWork с Репозиториями
+    /// </summary>
+    public class UnitOfWork : IDisposable
+    {
+        private ApplicationDbContext _context;
+        private Repository<Advert> _advertRepository;
+        private Repository<Brand> _brandRepository;
+        private Repository<Model> _modelRepository;
+        private Repository<CarCharacteristics> _carCharacteristicsRepository;
+        private Repository<Comment> _commentRepository;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Repository<Advert> AdvertRepository
+        {
+            get
+            {
+                if (_advertRepository == null)
+                    _advertRepository = new Repository<Advert>(_context);
+                return _advertRepository;
+            }
+        }
+        public Repository<Brand> BrandRepository
+        {
+            get
+            {
+                if (_brandRepository == null)
+                    _brandRepository = new Repository<Brand>(_context);
+                return _brandRepository;
+            }
+        }
+        public Repository<Model> ModelRepository
+        {
+            get
+            {
+                if (_modelRepository == null)
+                    _modelRepository = new Repository<Model>(_context);
+                return _modelRepository;
+            }
+        }
+        public Repository<CarCharacteristics> CarCharacteristicsRepository
+        {
+            get
+            {
+                if (_carCharacteristicsRepository == null)
+                    _carCharacteristicsRepository = new Repository<CarCharacteristics>(_context);
+                return _carCharacteristicsRepository;
+            }
+        }
+        public Repository<Comment> CommentRepository
+        {
+            get
+            {
+                if (_commentRepository == null)
+                    _commentRepository = new Repository<Comment>(_context);
+                return _commentRepository;
+            }
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }  
+}
